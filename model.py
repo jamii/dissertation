e14e71a7bcaf798ae1f9023207907fe5ff63b862
jamiiecb <jamiiecb@googlemail.com>
2010-05-27 16:05:46 +0000

    Untested python model generator
    Ignore-this: b499af2b036017daa623f73b01f047ed
    
    darcs-hash:20100527160546-b18dd-0c31d97db482c9b1a1b82f3a506541fa977cfd39.gz

---
+++ model.py
@@ -0,0 +1,105 @@
+header = """
+ctmc
+
+const double forget;
+const double cache;
+const double push_self;
+const double push;
+const double pull;
+
+"""
+
+def init(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            if i!=j:
+                yield ("edge_%d_%d : [0..1] init 0;" % (i,j))
+
+def forget(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            if i!=j:
+                yield ("[] (forget>0) & (edge_%d_%d=1) -> forget:(edge_%d_%d'=0);" % (i,j,i,j))
+
+def cache(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            if i!=j:
+                yield ("[] (cache>0) & (edge_%d_%d=0) -> cache:(edge_%d_%d'=1);" % (i,j,i,j))
+
+def push_self(n):
+    for i in range(1,n):
+        for j in range(1,n):
+            if i!=j:
+                yield("[] (push_self>0) & (edge_%d_%d=1) -> push_self:(edge_%d_%d'=1)" % (i,j,j,i))
+
+# push and pull may need to be adjusted for implementation - currently proportional to number of possible 2 edge combinations
+
+def push(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            for k in range(0,n):
+                if i!=j and i!=k and j!=k:
+                    yield ("[] (push>0) & (edge_%d_%d=1) & (edge_%d_%d=1) -> push:(edge_%d_%d'=1);" % (i,j,i,k,j,k))
+
+def pull(n):
+    for i in range(1,n):
+        for j in range(1,n):
+            for k in range(0,n):
+                if i!=j and i!=k and j!=k:
+                    yield ("[] (pull>0) & (edge_%d_%d=1) & (edge_%d_%d=1) -> pull:(edge_%d_%d'=1)" % (i,j,j,k,i,k))
+
+def time():
+    return """
+        rewards "time"
+            true:1;
+        endrewards
+    """
+
+def indegree(n):
+    edges_in = ["edge_%d_0" % i for i in range(0,n)]
+    return """
+        rewards "indegree"
+            true:%s;
+        endrewards
+    """ % " + ".join(edges_in)
+
+def outdegree(n):
+    edges_out = ["edge_0_%d" % i for i in range(0,n)]
+    return """
+        rewards "outdegree"
+            true:%s;
+        endrewards
+    """ % " + ".join(edges_out)
+
+def density(n):
+    edges =
+        for i in range(0,n):
+            for j in range(0,n):
+                if i!=j:
+                    yield ("edge_%d_%d" % (i,j))
+    return """
+        rewards "density"
+            true:%s;
+        endrewards
+    """ % " + ".join(edges)
+
+def main(n):
+    return "\n".join(
+        [header, "module edges"] +
+        init(n) +
+        forget(n) +
+        cache(n) +
+        push_self(n) +
+        push(n) +
+        pull(n) +
+        ["endmodule"] +
+        time() +
+        indegree(n) +
+        outdegree(n) +
+        density(n)
+    )
+
+if __name__ == '__main__':
+    print main(3)
+          
