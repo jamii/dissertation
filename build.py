e8ad9b51306fd2c797f2309fb51c23086a9f3c1a
jamiiecb <jamiiecb@googlemail.com>
2010-08-25 18:59:42 +0000

    Added a build script
    Ignore-this: 3d063d3da75697e6db55726540686f41
    
    darcs-hash:20100825185942-b18dd-1ceec302e64e1fdef86cdb054301251233d214fa.gz

---
+++ build.py
@@ -0,0 +1,20 @@
+from models.gen_model import *
+
+def write_model(name, contents):
+    with open('models/' + name + '.sm', 'w') as file:
+        file.write(contents)
+
+def main():
+    write_model("dtmc_single_no_past", dtmc_single(3, use_past=False))
+    write_model("dtmc_single", dtmc_single(3))
+    write_model("dtmc_multiple", dtmc_multiple(3))
+    write_model("dtmc_broken", dtmc_broken(3))
+    write_model("dtmc_full", dtmc_full(3))
+    write_model("ctmc_single", ctmc_single(3))
+    write_model("ctmc_multiple", ctmc_multiple(3))
+    write_model("ctmc_broken", ctmc_broken(3))
+    write_model("ctmc_full", ctmc_full(3))
+    
+
+if __name__ == '__main__':
+    main()
